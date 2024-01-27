abfcbd9894a0549053dd51b5e615abaf9f1e67d2
kiiiu15 <tinbulzomi@hotmail.com>
2024-01-27 02:24:49 -0300

    formating

--- my-vite-app/src/Pages/ListPeople.tsx
+++ my-vite-app/src/Pages/ListPeople.tsx
@@ -7,20 +7,19 @@ export function ListPeople() {
     usePeople();
   return (
     <>
-
-        <div className="p-2 py-3 bg-light">
-          {arePeopleLoading ? (
-            <Loading />
-          ) : (
-            <>
-              {didPeopleFetchFailed ? (
-                <p>{fetchPeopleError}</p>
-              ) : (
-                <PersonTable people={people} />
-              )}
-            </>
-          )}
-        </div>
+      <div className="p-2 py-3 bg-light">
+        {arePeopleLoading ? (
+          <Loading />
+        ) : (
+          <>
+            {didPeopleFetchFailed ? (
+              <p>{fetchPeopleError}</p>
+            ) : (
+              <PersonTable people={people} />
+            )}
+          </>
+        )}
+      </div>
     </>
   );
 }
