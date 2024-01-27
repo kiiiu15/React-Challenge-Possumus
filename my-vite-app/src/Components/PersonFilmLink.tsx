86709c5983490d9f1ef304b0d900bb8b5f8df403
kiiiu15 <tinbulzomi@hotmail.com>
2024-01-27 02:23:21 -0300

    create useFilmHook

--- my-vite-app/src/Components/PersonFilmLink.tsx
+++ my-vite-app/src/Components/PersonFilmLink.tsx
@@ -1,23 +1,23 @@
-import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { Film } from "../Model/Film";
-import { getFilmById } from "../Services/Films/getById";
+import { useFilm } from "../Hooks/useFilm";
 
 type Props = {
   filmUrl: string;
 };
 export function PersonFilmLink({ filmUrl }: Props) {
-  const [filmData, setFilmData] = useState<Film>();
-
-  useEffect(() => {
-    const filmId = filmUrl.split("/")[5];
-
-    getFilmById(filmId).then((filmData) => setFilmData(filmData));
-  }, [filmUrl]);
+  const filmId = filmUrl.split("/")[5];
+  const { filmData } = useFilm(filmId);
 
   return (
     <>
-      <Link className={`link-underline-opacity-0 ${(filmData?.title) ? "" : "placeholder bg-secondary col-12"}`} to={""}>{filmData?.title}</Link>
+      <Link
+        className={`link-underline-opacity-0 ${
+          filmData?.title ? "" : "placeholder bg-secondary col-12"
+        }`}
+        to={""}
+      >
+        {filmData?.title}
+      </Link>
     </>
   );
 }
